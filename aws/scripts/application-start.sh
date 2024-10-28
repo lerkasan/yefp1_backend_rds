21bc920467e5d7a175160c752c2c527043763b21
Valeriia Lapytska <lerkasan@gmail.com>
2024-10-28 09:52:59 -0400

    refactoring variable names

--- aws/scripts/application-start.sh
+++ aws/scripts/application-start.sh
@@ -1,43 +1,47 @@
 #!/bin/bash
 set -xe
 
-APPLICATION_NAME="yefp1"
-DEPLOYMENT_GROUP_NAME="stage"
-APP_DIR=/home/ubuntu/backend_rds
+PROJECT_NAME="yefp1"
+APPLICATION_NAME="backend_rds"
+DEPLOYMENT_GROUP_NAME="stage_${APPLICATION_NAME}"
+APP_DIR="/home/ubuntu/${APPLICATION_NAME}"
 
 CORS_ALLOWED_ORIGINS="http://localhost"
 DEBUG=False
-DB_PORT=5432
 
 AWS_REGION=$(curl -s http://169.254.169.254/latest/meta-data/placement/region)
 export AWS_REGION="${AWS_REGION:-us-east-1}"
 
 AWS_ACCOUNT_ID=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .accountId)
 
-DEPLOYMENT_ID=$(aws deploy list-deployments --application-name "$APPLICATION_NAME" --deployment-group-name "$DEPLOYMENT_GROUP_NAME" --region "$AWS_REGION" --include-only-statuses "InProgress" --query "deployments[0]" --output text --no-paginate)
+DEPLOYMENT_ID=$(aws deploy list-deployments --application-name "${PROJECT_NAME}_${APPLICATION_NAME}" --deployment-group-name "$DEPLOYMENT_GROUP_NAME" --region "$AWS_REGION" --include-only-statuses "InProgress" --query "deployments[0]" --output text --no-paginate)
 
 COMMIT_SHA=$(aws deploy get-deployment --deployment-id "$DEPLOYMENT_ID" --query "deploymentInfo.revision.gitHubLocation.commitId" --output text)
 
 BACKEND_RDS_TAG="${COMMIT_SHA:-latest}"
 
-DB_HOST=$(aws ssm get-parameter --region "$AWS_REGION" --name "${APPLICATION_NAME}_database_host" --with-decryption --query Parameter.Value --output text)
-DB_NAME=$(aws ssm get-parameter --region "$AWS_REGION" --name "${APPLICATION_NAME}_database_name" --with-decryption --query Parameter.Value --output text)
-DB_USER=$(aws ssm get-parameter --region "$AWS_REGION" --name "${APPLICATION_NAME}_database_username" --with-decryption --query Parameter.Value --output text)
-DB_PASSWORD=$(aws ssm get-parameter --region "$AWS_REGION" --name "${APPLICATION_NAME}_database_password" --with-decryption --query Parameter.Value --output text)
-SECRET_KEY=$(aws ssm get-parameter --region "$AWS_REGION" --name "${APPLICATION_NAME}_api_secret_key" --with-decryption --query Parameter.Value --output text)
+DB_HOST=$(aws ssm get-parameter --region "$AWS_REGION" --name "${PROJECT_NAME}_database_host" --with-decryption --query Parameter.Value --output text)
+DB_PORT=$(aws ssm get-parameter --region "$AWS_REGION" --name "${PROJECT_NAME}_database_port" --with-decryption --query Parameter.Value --output text)
+DB_NAME=$(aws ssm get-parameter --region "$AWS_REGION" --name "${PROJECT_NAME}_database_name" --with-decryption --query Parameter.Value --output text)
+DB_USER=$(aws ssm get-parameter --region "$AWS_REGION" --name "${PROJECT_NAME}_database_username" --with-decryption --query Parameter.Value --output text)
+DB_PASSWORD=$(aws ssm get-parameter --region "$AWS_REGION" --name "${PROJECT_NAME}_database_password" --with-decryption --query Parameter.Value --output text)
+SECRET_KEY=$(aws ssm get-parameter --region "$AWS_REGION" --name "${PROJECT_NAME}_api_secret_key" --with-decryption --query Parameter.Value --output text)
 
 export DB_HOST="$DB_HOST"
+export DB_PORT="${DB_PORT:-5432}"
 export DB_NAME="$DB_NAME"
 export DB_USER="$DB_USER"
 export DB_PASSWORD="$DB_PASSWORD"
 export DB_PORT="$DB_PORT"
 
-export SECRET_KEY="$SECRET_KEY"
-export CORS_ALLOWED_ORIGINS="$CORS_ALLOWED_ORIGINS"
-export DEBUG="$DEBUG"
+export API_SECRET_KEY="$SECRET_KEY"
+export API_CORS_ALLOWED_ORIGINS="$CORS_ALLOWED_ORIGINS"
+export API_DEBUG="${DEBUG:-False}"
 
 export BACKEND_RDS_TAG="${BACKEND_RDS_TAG:-latest}"
 export AWS_ACCOUNT_ID="$AWS_ACCOUNT_ID"
+
+aws ecr get-login-password --region "$AWS_REGION" | docker login --username AWS --password-stdin "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com"
 
 cd "$APP_DIR" || exit
 docker compose up -d
