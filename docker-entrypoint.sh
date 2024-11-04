f1dc76717a7364d01b5b5aa6d2a491ca07594b19
Valeriia Lapytska <lerkasan@gmail.com>
2024-11-03 20:59:38 -0500

    cleaned up commented out code

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,12 +7,6 @@ set -eou pipefail
 echo "Applying database migrations"
 python manage.py migrate
 
-# echo "Checking superuser"
-# DJANGO_SUPERUSER_EMAIL=$(cat /run/secrets/django_superuser_email) && \
-# DJANGO_SUPERUSER_USERNAME=$(cat /run/secrets/django_superuser_username) && \
-# DJANGO_SUPERUSER_PASSWORD=$(cat /run/secrets/django_superuser_password) && \
-# python manage.py createsuperuser --no-input --username "$DJANGO_SUPERUSER_USERNAME" --email "$DJANGO_SUPERUSER_EMAIL" || true
-
 CPU_CORES=$(grep ^cpu\\scores /proc/cpuinfo | uniq | awk '{print $4}')
 WORKERS=$(($CPU_CORES * 2 + 1))
 
